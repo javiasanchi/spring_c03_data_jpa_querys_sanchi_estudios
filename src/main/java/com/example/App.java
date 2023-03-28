bf56900f641e45d201a3e0cc216621cfdcab1af6
Javier <javi.ssweb@gmail.com>
2023-03-28 12:38:10 +0200

    Refactor App y añadir application properties

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringC03DataJpaQuerysSanchiEstudiosApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringC03DataJpaQuerysSanchiEstudiosApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
