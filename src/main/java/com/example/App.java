39c88a911372dc1287a49c25650855d4b1eb94f9
Javier <javi.ssweb@gmail.com>
2023-03-30 20:56:56 +0200

    Filtros LocalDate caducidad

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -17,130 +17,141 @@ import java.util.List;
 @SpringBootApplication
 public class App {
 
-	static ApplicationContext context;
+    static ApplicationContext context;
 
-	public static void main(String[] args) {
-		context = SpringApplication.run(App.class, args);
+    public static void main(String[] args) {
+        context = SpringApplication.run(App.class, args);
 
-		datosDemo();
-		findAll();;
-		findAllByNombre();
-		findAllByCif();
-		findAllByTelefono();
-		findAllByEmail();
-		filtroPorCalleYPais();
-		// filtroPrecioMax();
-
-	}
-	/*private static void filtroPrecioMax(){
-		ProductosRepository productosRepo = context.getBean((ProductosRepository.class));
-
-		System.out.println("==================== FIND ALL BY PRECIO MÁXIMO ====================");
-		List<Productos> productos = productosRepo.findAllByPrecio(12.70);
-
-		for (Productos product: productos) {
-
-			System.out.println(product);
-
-		}
-	}*/
-	private static void filtroPorCalleYPais() {
-		DireccionRepository direccionRepo = context.getBean(DireccionRepository.class);
-		System.out.println("==================== FIND ALL BY CALLE Y PAIS ====================");
-		List<Direccion> direccionFiltro = direccionRepo.findAllByCalleAndPais("calle2", "pais3");
-
-		for (Direccion direccion: direccionFiltro) {
-			System.out.println(direccion);
-		}
-
-	}
-	private static void findAll() {
-
-			TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
-			System.out.println("==================== FIND ALL ====================");
-
-			List<Tienda> tiendas = tiendaRepository.findAll();{
-
-				for (Tienda tienda: tiendas) {
-					System.out.println("Las tiendas son :" + tienda);
-				}
-
-			}
-
-		}
-		public static void findAllByNombre() {
-			TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
-			System.out.println("==================== FIND ALL BY NOMBRE ====================");
-			List<Tienda> tiendas1 = tiendaRepository.findAllByNombre("Expert");
-			System.out.println("La tienda por NOMBRE es : " + tiendas1);
-
-		}
-
-		public static void findAllByCif() {
-			TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
-			System.out.println("==================== FIND ALL BY CIF ====================");
-			List<Tienda> tiendas2 = tiendaRepository.findAllByCif("08032071");
-			System.out.println("La tienda por CIF es :" + tiendas2);
-
-		}
-
-		public static void findAllByTelefono() {
-			TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
-			System.out.println("==================== FIND ALL BY TELEFONO ====================");
-			List<Tienda> tiendas3 = tiendaRepository.findAllByTelefono(678050472);
-			System.out.println("La tienda por telefo es :" + tiendas3);
-
-		}
-
-		public static void findAllByEmail() {
-			TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
-			System.out.println("==================== FIND ALL BY EMAIL ====================");
-			List<Tienda> tiendas4 = tiendaRepository.findAllByEmail("javi@javi.es");
-			System.out.println("La tienda por email es :" + tiendas4);
-
-		}
-
-	public static  void datosDemo() {
-
-		TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
-		DireccionRepository direccionRepository = context.getBean(DireccionRepository.class);
-		ProductosRepository productosRepository = context.getBean(ProductosRepository.class);
+        datosDemo();
+        findAll();
+        findAllByNombre();
+        findAllByCif();
+        findAllByTelefono();
+        findAllByEmail();
+        filtroPorCalleYPais();
+        filtroPorPrecioEntre();
+        filtroPorCaducidadEntre ();
 
 
-		Tienda tien1 = new Tienda("Expert", "08032071", 678050472, "javi@expert.es", 1245.98, true);
-		Direccion dir1 = new Direccion("calle1", 45, "local1", "ciudad1", "pais1", 28000);
+    }
 
-		Tienda tien2 = new Tienda("Manolin", "49783634", 69000000, "manolin@manolin.com", 45.98, true);
-		Direccion dir2 = new Direccion("calle2", 5, "local2", "ciudad2", "pais2", 27400);
+    private static void filtroPorCaducidadEntre (){
+        ProductosRepository productosRepository = context.getBean(ProductosRepository.class);
+        List<Productos> caducidadEntre = productosRepository.findAllByCaducidadBefore(LocalDate.of(2035,
+                Month.JANUARY,1));
+        for (Productos productos: caducidadEntre) {
+            System.out.println("Caducan en :" + productos);
 
-		Tienda tien3 = new Tienda("Estanco", "40935224", 916440000, "estanco@estanco.com", 489.98, true);
-		Direccion dir3 = new Direccion("calle3", 6, "local3", "ciudad3", "pais3", 20400);
+        }
+    }
+    private static void filtroPorPrecioEntre(){
+        ProductosRepository productosRepository = context.getBean(ProductosRepository.class);
+        List<Productos> precioEntre = productosRepository.findAllByPrecioBetween(45D, 250D);
 
-		Tienda tien4 = new Tienda("Panadería", "73529407", 636195753, "javi@javi.es", 489.98, true);
-		Direccion dir4 = new Direccion("calle2", 6, "local3", "ciudad3", "pais3", 50400);
+        for (Productos productos: precioEntre){
+            System.out.println("Los productos son :" + productos);
+        }
+    }
+    private static void filtroPorCalleYPais() {
+        DireccionRepository direccionRepo = context.getBean(DireccionRepository.class);
+        System.out.println("==================== FIND ALL BY CALLE Y PAIS ====================");
+        List<Direccion> direccionFiltro = direccionRepo.findAllByCalleAndPais("calle2", "pais3");
 
-		tien1.setDireccion(dir1);
-		tien2.setDireccion(dir2);
-		tien3.setDireccion(dir3);
-		tien4.setDireccion(dir3);
+        for (Direccion direccion : direccionFiltro) {
+            System.out.println(direccion);
+        }
 
-		direccionRepository.saveAll(List.of(dir1, dir2,dir3,dir4));
-		tiendaRepository.saveAll(List.of(tien1, tien2,tien3,tien4));
+    }
 
-		var prod1 = new Productos("prod1", "0001", 125.70, 25, true, LocalDate.of(2024, Month.APRIL, 23));
-		var prod2 = new Productos("prod2", "0002", 12.70, 500, true, LocalDate.of(2025, Month.MAY, 3));
-		var prod3 = new Productos("prod3", "0003", 1200.0, 50, true, LocalDate.of(2045, Month.JANUARY, 30));
-		var prod4 = new Productos("prod4", "0004", 16780.0, 20, true, LocalDate.of(2035, Month.NOVEMBER, 20));
+    private static void findAll() {
 
-		prod1.getTienda().addAll(List.of(tien1, tien2));
-		prod2.getTienda().add(tien1);
-		prod3.getTienda().addAll(List.of(tien1, tien2));
-		prod4.getTienda().add(tien2);
+        TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
+        System.out.println("==================== FIND ALL ====================");
 
-		productosRepository.saveAll(List.of(prod1, prod2, prod3, prod4));
+        List<Tienda> tiendas = tiendaRepository.findAll();
+        {
 
-	}
+            for (Tienda tienda : tiendas) {
+                System.out.println("Las tiendas son :" + tienda);
+            }
 
-	}
+        }
+
+    }
+
+    public static void findAllByNombre() {
+        TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
+        System.out.println("==================== FIND ALL BY NOMBRE ====================");
+        List<Tienda> tiendas1 = tiendaRepository.findAllByNombre("Expert");
+        System.out.println("La tienda por NOMBRE es : " + tiendas1);
+
+    }
+
+    public static void findAllByCif() {
+        TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
+        System.out.println("==================== FIND ALL BY CIF ====================");
+        List<Tienda> tiendas2 = tiendaRepository.findAllByCif("08032071");
+        System.out.println("La tienda por CIF es :" + tiendas2);
+
+    }
+
+    public static void findAllByTelefono() {
+        TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
+        System.out.println("==================== FIND ALL BY TELEFONO ====================");
+        List<Tienda> tiendas3 = tiendaRepository.findAllByTelefono(678050472);
+        System.out.println("La tienda por telefo es :" + tiendas3);
+
+    }
+
+    public static void findAllByEmail() {
+        TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
+        System.out.println("==================== FIND ALL BY EMAIL ====================");
+        List<Tienda> tiendas4 = tiendaRepository.findAllByEmail("javi@javi.es");
+        System.out.println("La tienda por email es :" + tiendas4);
+
+    }
+
+    public static void datosDemo() {
+
+        TiendaRepository tiendaRepository = context.getBean(TiendaRepository.class);
+        DireccionRepository direccionRepository = context.getBean(DireccionRepository.class);
+        ProductosRepository productosRepository = context.getBean(ProductosRepository.class);
+
+
+        Tienda tien1 = new Tienda("Expert", "08032071", 678050472, "javi@expert.es", 1245.98, true);
+        Direccion dir1 = new Direccion("calle1", 45, "local1", "ciudad1", "pais1", 28000);
+
+        Tienda tien2 = new Tienda("Manolin", "49783634", 69000000, "manolin@manolin.com", 45.98, true);
+        Direccion dir2 = new Direccion("calle2", 5, "local2", "ciudad2", "pais2", 27400);
+
+        Tienda tien3 = new Tienda("Estanco", "40935224", 916440000, "estanco@estanco.com", 489.98, true);
+        Direccion dir3 = new Direccion("calle3", 6, "local3", "ciudad3", "pais3", 20400);
+
+        Tienda tien4 = new Tienda("Panadería", "73529407", 636195753, "javi@javi.es", 489.98, true);
+        Direccion dir4 = new Direccion("calle2", 6, "local3", "ciudad3", "pais3", 50400);
+
+        tien1.setDireccion(dir1);
+        tien2.setDireccion(dir2);
+        tien3.setDireccion(dir3);
+        tien4.setDireccion(dir3);
+
+        direccionRepository.saveAll(List.of(dir1, dir2, dir3, dir4));
+        tiendaRepository.saveAll(List.of(tien1, tien2, tien3, tien4));
+
+        var prod1 = new Productos("prod1", "0001", 125.70, 25, true, LocalDate.of(2024, Month.APRIL, 23));
+        var prod2 = new Productos("prod2", "0002", 52.70, 500, true, LocalDate.of(2025, Month.MAY, 3));
+        var prod3 = new Productos("prod3", "0003", 1200.0, 50, true, LocalDate.of(2045, Month.JANUARY, 30));
+        var prod4 = new Productos("prod4", "0004", 16780.0, 20, true, LocalDate.of(2035, Month.NOVEMBER, 20));
+
+        prod1.getTienda().addAll(List.of(tien1, tien2));
+        prod2.getTienda().add(tien1);
+        prod3.getTienda().addAll(List.of(tien1, tien2));
+        prod4.getTienda().add(tien2);
+
+        productosRepository.saveAll(List.of(prod1, prod2, prod3, prod4));
+
+    }
+
+}
 
 
--- src/main/java/com/example/repositories/ProductosRepository.java
+++ src/main/java/com/example/repositories/ProductosRepository.java
@@ -3,8 +3,12 @@ package com.example.repositories;
 import com.example.entities.Productos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ProductosRepository extends JpaRepository<Productos, Long> {
-    List<Productos> findAllByPrecio (Double precio);
+    List<Productos> findAllByPrecioBetween (Double precioMin, Double precioMax);
+    List<Productos> findAllByCaducidadBefore (LocalDate caducado);
+
+
 }
